84cf1f1cae0f66cb50b302e0a281b27e8b0f8bbc
kimiscirle <kmiscircle@gmail.com>
2014-02-27 18:15:11 +0800

    add changes from olive

--- mvcp/src/edu/hebtu/movingcampus/activity/base/ActionDispatcher.java
+++
@@ -1,7 +0,0 @@
-package edu.hebtu.movingcampus.activity.base;
-
-import android.view.MotionEvent;
-
-public interface ActionDispatcher {
-	public boolean dispatchTouchEvent(MotionEvent event);
-}
--- mvcp/src/edu/hebtu/movingcampus/subject/base/OneofNews.java
+++
@@ -1,8 +0,0 @@
-package edu.hebtu.movingcampus.subject.base;
-
-import android.app.Activity;
-import edu.hebtu.movingcampus.entity.NewsShort;
-
-public interface OneofNews {
-	public NewsShort dump(Activity context);
-}
